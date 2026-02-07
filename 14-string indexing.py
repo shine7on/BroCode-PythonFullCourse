61cafd8e97e56f272986df1c111a749c41d54216
shine7on <pabboo13@gmail.com>
2026-02-07 11:32:06 -0600

    Learned string indexing [start:end:step]

---
+++ 14-string indexing.py
@@ -0,0 +1,13 @@
+# indexing
+# accessing elements of sequence using []
+
+credit_num = "1234-5678-9012-3456"
+
+print(credit_num[0])
+print(credit_num[0:4])
+
+print(credit_num[-3])
+print(credit_num[-4:])
+
+print(credit_num[::4])
+print(credit_num[::-1])
