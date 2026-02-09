12a76bb374cb784805aa79bf5897d1e9281de0bb
shine7on <pabboo13@gmail.com>
2026-02-09 15:02:25 -0600

    Learned modules (import MODULE_NAME)

---
+++ 39-modules.py
@@ -0,0 +1,23 @@
+# modules
+# file containing code you want to include
+
+import math
+import math as m
+from math import pi
+import example
+
+print(math.pi)
+print(m.pi)
+print(pi)
+
+print(math.e)
+
+
+print(example.circumference(4))
+print(example.cube(3))
+
+
+
+
+
+
---
+++ example.py
@@ -0,0 +1,15 @@
+pi = 3.14159
+
+def square (x):
+    return x**2
+
+def cube (x):
+    return x ** 3
+
+
+def circumference (r):
+    return 2 * pi * r
+
+def area (r):
+    return pi * r**2
+
