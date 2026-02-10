021811bee7a00006adad684486908bde2b7e5af0
shine7on <pabboo13@gmail.com>
2026-02-09 21:27:48 -0600

    Add main() in banking program

--- 42-banking program.py
+++ 42-banking program.py
@@ -1,13 +1,9 @@
 # Python Banking Program
 
-balance = 0
-is_running = True
-
-def show_balance():
+def show_balance(balance):
     print(f"Your balance is ${balance:.2f}")
 
-def deposit():
-    global balance
+def deposit(balance):
 
     amount = float(input("Enter an amount to be deposited: $"))
 
@@ -16,9 +12,10 @@ def deposit():
     else:
         balance += amount
         print(f"Your balance is ${balance:.2f}")
+    
+    return balance
 
-def withdraw():
-    global balance
+def withdraw(balance):
 
     amount = float(input("Enter an amount to be withdrawn: $"))
 
@@ -28,30 +25,41 @@ def withdraw():
         balance -= amount
         print(f"Your balance is ${balance:.2f}")
 
+    return balance
 
 
 
-while is_running:
-    print("Banking Program")
-    print("1: show balance")
-    print("2: deposit")
-    print("3: withdraw")
-    print("4: exit")
+def main():
+    balance = 0
+    is_running = True
 
-    choice = int(input("Enter your choice (1 - 4): "))
+    while is_running:
+        print("*******************")
+        print("Banking Program")
+        print("*******************")
+        print("1: show balance")
+        print("2: deposit")
+        print("3: withdraw")
+        print("4: exit")
 
-    match choice:
-        case 1:
-            show_balance()
-        case 2:
-            deposit()
-        case 3:
-            withdraw()
-        case 4:
-            is_running = False
-        case _:
-            print("Invalid")
-    
+        choice = int(input("Enter your choice (1 - 4): "))
 
-print("Thank you. Have a nice day.")
+        match choice:
+            case 1:
+                show_balance(balance)
+            case 2:
+                balance = deposit(balance)
+            case 3:
+                balance = withdraw(balance)
+            case 4:
+                is_running = False
+            case _:
+                print("Invalid")
+        
+        print("****************")
+        
 
+    print("Thank you. Have a nice day.")
+
+if __name__ == "__main__":
+    main()
