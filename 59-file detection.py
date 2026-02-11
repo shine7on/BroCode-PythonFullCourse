86b207d5bc2202c880594330d0c1f1e386cb6fd6
shine7on <pabboo13@gmail.com>
2026-02-11 13:41:09 -0600

    Learned file detection [import os]

---
+++ 59-file detection.py
@@ -0,0 +1,17 @@
+# Python file detection
+
+import os
+
+file_path = "test.txt"
+
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+
+    if os.path.isfile(file_path):
+        print("It is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+else:
+    print("The location doesn't exist")
+
