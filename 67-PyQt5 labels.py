b97e61eff21ddb37bac00b0c275f0b28de63a478
shine7on <pabboo13@gmail.com>
2026-02-12 11:25:56 -0600

    Learned PyQt5 Labels

---
+++ 67-PyQt5 labels.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(500,300,500,500)
+        
+        label = QLabel("Hello",self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0,0,500,100)
+        label.setStyleSheet("color: white;" "background-color: pink;" "font-weight: bold;")
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+    
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
